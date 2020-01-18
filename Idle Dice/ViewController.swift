ae07a330e863624a49bbb6459dd14b7d1ae0b850
Wesley Dashner <wdashner11@gmail.com>
2020-01-18 15:02:27 -0700

    initial commit

---
+++ Idle Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Idle Dice
+//
+//  Created by Wesley Dashner on 1/18/20.
+//  Copyright © 2020 Wesley Dashner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
